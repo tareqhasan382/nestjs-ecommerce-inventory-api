cae84c914d65c1820fcf653d820520d35f36b096
Tareq Hasan <tareqhasan382@gmail.com>
2025-09-20 11:52:26 +0600

    check cccc

--- src/main.ts
+++ src/main.ts
@@ -4,27 +4,30 @@ import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 //import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  try {
+    const app = await NestFactory.create(AppModule);
 
-  // Swagger configuration
-  const config = new DocumentBuilder()
-    .setTitle('E-Commerce Inventory API')
-    .setDescription('API documentation for inventory management')
-    .setVersion('1.0')
-    .addBearerAuth() // for JWT auth
-    .build();
+    // Swagger configuration
+    const config = new DocumentBuilder()
+      .setTitle('E-Commerce Inventory API')
+      .setDescription('API documentation for inventory management')
+      .setVersion('1.0')
+      .addBearerAuth() // for JWT auth
+      .build();
 
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api-docs', app, document);
-  // app.useGlobalPipes(
-  //   new ValidationPipe({
-  //     whitelist: true, // strip unknown properties
-  //     forbidNonWhitelisted: true, // error on extra fields
-  //     transform: true, // transform types (e.g. strings to numbers)
-  //   }),
-  // );
-
-
-  await app.listen(process.env.PORT || 5000);
+    const document = SwaggerModule.createDocument(app, config);
+    SwaggerModule.setup('api-docs', app, document);
+    // app.useGlobalPipes(
+    //   new ValidationPipe({
+    //     whitelist: true, // strip unknown properties
+    //     forbidNonWhitelisted: true, // error on extra fields
+    //     transform: true, // transform types (e.g. strings to numbers)
+    //   }),
+    // );
+    await app.listen(3000);
+  } catch (err) {
+    console.error('Error during bootstrap', err);
+    process.exit(1);
+  }
 }
 bootstrap();
